c9145d48d51d93f1aac21fe361fa254fe3c1c710
Thom <tallen.thom@gmail.com>
2016-03-16 13:43:52 -0600

    Trying to display the already pared devices. Having problems with the ArrayAdapter.

--- app/src/main/java/thom/exotics/com/rockpaperscissors/BluetoothConnectionHandler.java
+++ app/src/main/java/thom/exotics/com/rockpaperscissors/BluetoothConnectionHandler.java
@@ -2,8 +2,13 @@ package thom.exotics.com.rockpaperscissors;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Set;
 
 import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
@@ -11,15 +16,45 @@ import static android.support.v4.app.ActivityCompat.startActivityForResult;
  * Created by Thom on 3/14/2016.
  */
 public class BluetoothConnectionHandler {
+    Activity theActivity;
 
+    ArrayAdapter mArrayAdapter;
+
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    public BluetoothConnectionHandler() {
+        // Do nothing for now
+    }
 
     public BluetoothConnectionHandler(Activity activity) {
-        Activity theActivity = activity;
+        theActivity = activity;
 
-        // Do nothing for now
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            System.out.println("DeBug - Device does not support Bluetooth");
+        } else {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                theActivity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
 
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+//                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+//                System.out.println(device.getName() + " <-> " + device.getAddress());
+            }
+        }
 
     }
 
+    public ArrayAdapter getMArrayAdapter(){
+        return mArrayAdapter;
+    }
 
 }
--- app/src/main/java/thom/exotics/com/rockpaperscissors/MainActivity.java
+++ app/src/main/java/thom/exotics/com/rockpaperscissors/MainActivity.java
@@ -2,22 +2,32 @@ package thom.exotics.com.rockpaperscissors;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private final static int REQUEST_ENABLE_BT = 1;
+    BluetoothConnectionHandler btConnection;
+    ListView deviceList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        btConnection = new BluetoothConnectionHandler();
+
+        deviceList = (ListView) findViewById(R.id.listView);
         Button btnHost = (Button) findViewById(R.id.hostClick);
         Button btnJoin = (Button) findViewById(R.id.joinClick);
 
@@ -30,7 +40,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         // check that Bluetooth is on
-        bluetoothConnection();
+
+        btConnection = new BluetoothConnectionHandler(this);
+        deviceList.setAdapter(btConnection.getMArrayAdapter());
 
         switch (v.getId()) {
             case R.id.hostClick:
@@ -40,21 +52,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.joinClick:
                 // TODO: create a bluetooth client connection to the server
                 System.out.println("DeBug - Join was clicked");
+                break;
         }
-    }
-
-    public void bluetoothConnection() {
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-            System.out.println("DeBug - Device does not support Bluetooth");
-        } else {
-            if (!mBluetoothAdapter.isEnabled()) {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            }
-        }
-
     }
 
 }
